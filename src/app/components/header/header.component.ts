52c3bb2dd65ad7f0693b15725af279b3192dd899
lisa13 <lisamanukyan@gmail.com>
2020-02-29 10:36:43 +0400

    changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public title = 'Edit and Preview Status Report';
+  public platform = 'Osberg B';
+  public well= '30/9-B-30-D-T-2 ';
 
   constructor() { }
 
